93a3229e0ec3daf12a2cecb855ccd861f1d372df
Dat Pham <dpham@apple.com>
2015-03-31 16:39:50 -0700

    remove comments

--- public/js/app/galleryPage.js
+++ public/js/app/galleryPage.js
@@ -124,62 +124,13 @@ var HomeComponent = React.createClass({
 
   },
   componentDidMount: function() {
-    //var _this = this;
-    //this.socket = io.connect(window.location.origin);
-    //this.socket.on('newInstagramPosts', function (response) {
-    //  $.each(response.newBundles, function(index, bundle) {
-    //    var url = instagramUrl.replace(/{tagId}/,bundle.object_id).replace(/{client_id}/,response.client_id);
-    //
-    //    $.ajax({
-    //      url: url,
-    //      dataType: 'jsonp',
-    //    }).done(function(posts){
-    //      var d = false;
-    //      var newPosts = [];
-    //      $.each(posts.data,function(index, post) {
-    //        var newPost = {
-    //          type : "INSTAGRAM",
-    //          mediaType : post.type,
-    //          userName : post.user.username,
-    //          fullName : post.user.full_name,
-    //          profilePictue : post.user.profile_picture,
-    //          picture :  post.images.standard_resolution.url,
-    //          video : post.type == "video" ? post.videos.standard_resolution.url : '',
-    //          caption : post.caption? post.caption.text : '',
-    //          time : post.created_time
-    //        }
-    //        newPosts.push(newPost);
-    //      });
-    //
-    //      var newState = _this.state.posts.concat(newPosts);
-    //      newState = _.uniq(newState, function(post) {
-    //        return post.userName+post.time;
-    //      });
-    //      newState = _.sortBy(newState, function(post){
-    //        return post.time
-    //      }).reverse();
-    //
-    //      if(newState.length > 100){
-    //        newState = newState.splice(0,100);
-    //      }
-    //      _this.setState({
-    //        posts : newState
-    //      })
-    //
-    //      console.log(_this.state.posts.length);
-    //      //_this.socket.disconnect();
-    //    })
-    //  });
-    //});
-
-    
+   
     this.isotope = $(this.refs.socialContainer.getDOMNode()).isotope({ 
       layoutMode: 'masonry',
       itemSelector: '.cell'
     });
 
     this.refs.socialContainer.getDOMNode().focus();
-    //this.imagesLoaded();
 
   },  
   componentDidUpdate: function(prevProps, prevState) {
